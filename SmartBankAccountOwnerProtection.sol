f01f90bcc036f1475f042824225dc534460d7f7c
parroz4 <31765354+parroz4@users.noreply.github.com>
2022-04-21 12:51:53 +0200

    Create SmartBankAccountOwnerProtection.sol
    Created using remix-ide: Realtime Ethereum Contract Compiler and Runtime. Load this file by pasting this gists URL or ID at https://remix.ethereum.org/#version=soljson-v0.8.11+commit.d7f03943.js&optimize=false&runs=200&gist=

---
+++ SmartBankAccountOwnerProtection.sol
@@ -0,0 +1,66 @@
+pragma solidity ^0.8.11;
+// pragma solidity >=0.7.0 <0.9.0;
+// SPDX-License-Identifier: MIT
+
+contract SmartBankAccount {
+
+    address owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the bank owner can perform this transaction");
+        // it's the account with which the contract is deployed
+        _;
+    }
+
+    uint totalContractBalance = 0;
+
+    function getContractBalance() public view onlyOwner returns(uint){
+        return totalContractBalance;
+    }
+    
+    mapping(address => uint) balances;
+    mapping(address => uint) depositTimestamps;
+    
+    function addBalance(address userAddress) public payable returns(address,uint,uint,uint){
+        balances[userAddress] = msg.value;
+        totalContractBalance = totalContractBalance + msg.value;
+        depositTimestamps[userAddress] = block.timestamp;
+        return (userAddress,depositTimestamps[userAddress],balances[userAddress],totalContractBalance);
+    }
+    
+    function getBalanceBank(address userAddress) public view onlyOwner returns(uint) {
+        uint principal = balances[userAddress];
+        uint timeElapsed = block.timestamp - depositTimestamps[userAddress]; //seconds
+        return principal + uint((principal * 7 * timeElapsed) / (100 * 365 * 24 * 60 * 60)) + 1; //simple interest of 0.07%  per year
+    }
+
+    function getBalanceOutsideBank(address userAddress) public view returns(uint) {
+        return userAddress.balance; 
+    }
+    
+    function withdraw(address userAddress) public payable {
+        address payable withdrawTo = payable(userAddress);
+        uint amountToTransfer = getBalanceBank(userAddress);
+        require(amountToTransfer < totalContractBalance,"insufficient funds on bank");
+        withdrawTo.transfer(amountToTransfer);
+        totalContractBalance = totalContractBalance - amountToTransfer;
+        balances[userAddress] = 0;
+    }
+
+    function withdrawAmount(uint amount,address userAddress) public payable{
+        address payable withdrawTo = payable(userAddress);
+        require(amount < getBalanceBank(userAddress),"insufficient funds on user account");
+        withdrawTo.transfer(amount);
+        balances[userAddress] -= amount;
+        totalContractBalance -= amount;
+    }
+    
+    function addMoneyToContract() public payable {
+        totalContractBalance += msg.value;
+    }
+
+}
